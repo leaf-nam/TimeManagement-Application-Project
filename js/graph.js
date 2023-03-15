1861941befbaf13f8bf43fa6c7bdae7615f0d782
Sangyeop Nam <spearoad15@gmail.com>
2023-03-15 18:02:24 +0900

    test : 움직이는 원 애니메이션 추가
    - 움직이는 원 애니메이션을 만들어서 To-do를 그릴 위치에 해당 원을
      위치시키고, 시간값에 맞춰 이동시킬 예정
    - 수직선은 차후 시간값에 맞춰 이동시키고, 수평선은 고정해서 To-Do의
      importancy를 표현할 예정

--- js/graph.js
+++ js/graph.js
@@ -1,47 +1,74 @@
 const canvas = document.querySelector("#todoCanvas");
 const canvasContainer = document.querySelector("#canvas-container");
 const queriedToDo = document.querySelector("#todo-list");
-const ctx = canvas.getContext('2d');
 const graphWidth = window.innerWidth;
 const graphHeight = 500;
-const img = new Image();
-img.src = `img/check.png`;
-
-function paintCheckTodo(todo){ // Todo를 웹페이지(html)에 그리기 위한 함수(변수는 todo객체)
-    const checkSize = 30
-    const remains = RemainTimeCalcurate(todo);
-    const locationX = graphWidth * (Number(remains[0])*60+Number(remains[1])) / (24*60);
-    const locationY = graphHeight * (5-todo.important) / 5;
-    console.log(todo);
-    img.onload = function(){
-        ctx.drawImage(img,locationX,locationY,checkSize,checkSize);
-        console.log(locationX);
-        console.log(locationY);
-    }
-}
+const checkImg = new Image();
+checkImg.src = `img/check.png`;
+const checkSize = 20;
 // 캔버스 설정
 canvas.width = graphWidth;
 canvas.height = graphHeight;
+const ctx = canvas.getContext('2d');
 
-// 세로줄
-ctx.strokeStyle = "green";
-ctx.lineWidth = 3;
-for (let i = 1; i <= 24; i++){
+let x = 100;
+let y = 100;
+let dx = 10;
+let dy = 10;
+const radius = 20;
+
+function drawFrame() {
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    // 세로줄
+    ctx.strokeStyle = "green";
+    ctx.lineWidth = 1;
+    for (let i = 1; i <= 24; i++){
+        ctx.beginPath();
+        ctx.moveTo(graphWidth/25*i, 0);
+        ctx.lineTo(graphWidth/25*i, graphHeight);
+        ctx.stroke();
+    }
+    for (let i = 1; i <= 10; i++){
+        ctx.beginPath();
+        ctx.moveTo(0, graphHeight/10*(2*i-1));
+        ctx.lineTo(graphWidth, graphHeight/10*(2*i-1));
+        ctx.stroke();
+    }
+    x += dx;
+    y += dy;
+    if (x + radius > canvas.width || x - radius < 0) {
+        dx = -dx;
+      }
+    if (y + radius > canvas.height || y - radius < 0) {
+        dy = -dy;
+      }
     ctx.beginPath();
-    ctx.moveTo(1920/24*i, 0);
-    ctx.lineTo(1920/24*i, canvas.height);
-    ctx.stroke();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    requestAnimationFrame(drawFrame);
 }
 
-// 가로줄
-ctx.beginPath();
-ctx.moveTo(0,0);
-ctx.lineTo(canvas.width, 0);
-ctx.stroke();
-ctx.beginPath();
-ctx.moveTo(0,canvas.height);
-ctx.lineTo(canvas.width, canvas.height);
-ctx.stroke();
+function calLocationTodo(todo){ // Todo 위치를 계산하기 위한 함수(변수는 todo객체)
+    const remains = RemainTimeCalcurate(todo);
+    const locationX = graphWidth * (Number(remains[0])*60+Number(remains[1])) / (26*60);
+    const locationY = graphHeight * (9-(todo.important-1)*2) / 10;
+    return [locationX, locationY];
+}
 
-parsedToDos = JSON.parse(savedToDos);
-parsedToDos.forEach(paintCheckTodo);
+
+
+requestAnimationFrame(drawFrame)
+//Todo 그리기(원)
+// parsedToDos = JSON.parse(savedToDos);
+// let locationTodos = []
+// parsedToDos.forEach((parsedTodo) => locationTodos.push(calLocationTodo(parsedTodo)));
+// locationTodos.forEach(function(locationTodo) {
+//     ctx.strokeStyle = "red";
+//     ctx.lineWidth = 10;
+//     ctx.beginPath();
+//     ctx.arc(locationTodo[0],locationTodo[1],checkSize,0,Math.PI*2);
+//     ctx.fillStyle = "yellow";
+//     ctx.fill();
+//     ctx.closePath();
+// })
--- js/todo.js
+++ js/todo.js
@@ -1,20 +1,20 @@
-const toDoForm = document.querySelector("#todo-form");  // "todo-form" 이라는 값을 DOM에서 찾아서 저장
-const toDoInput = toDoForm.querySelector("input[name='todo-text']");  // input 중 name이 todo-text인 값을 todoForm에서 찾아서 저장
-const stars = toDoForm.querySelector("#stars");  // id가 star인 값을 todoForm에서 찾아서 저장
+// HTML에서 요소 속성 불러오기
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = toDoForm.querySelector("input[name='todo-text']");
+const stars = toDoForm.querySelector("#stars");
 const toDoImportancy = stars.querySelectorAll("input[type='radio']");
-const toDoLimit = toDoForm.querySelector("input[name='todo-limit']");  // input 중 name이 todo-limit인 값을 todoForm에서 찾아서 저장
-const toDoList = document.querySelector("#todo-list");  // id가 "todo-list"인 값을 DOM에서 찾아서 저장
-const TODOS_KEY = "todos";  // localStorage에 저장되는 Todos의 KEY값
-let savedToDos = localStorage.getItem(TODOS_KEY);  // localStorage에 있는 Todo를 불러와 변수로 저장
+const toDoLimit = toDoForm.querySelector("input[name='todo-limit']");
+const toDoList = document.querySelector("#todo-list");
+// localStorage에 있는 Todo를 불러와 변수로 저장
+const TODOS_KEY = "todos"; 
+let savedToDos = localStorage.getItem(TODOS_KEY); 
 let parsedToDos = JSON.parse(savedToDos);
-let toDos = [];
-toDos = parsedToDos;
-
-function saveToDos() { // Todo를 localStorage에 저장하기 위한 함수
-  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+ // Todo를 localStorage에 저장하기 위한 함수
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(parsedToDos));
   savedToDos = localStorage.getItem(TODOS_KEY);
 }
-
+// Todo의 limit까지 남은시간을 계산하기 위한 함수
 function RemainTimeCalcurate(todo) {
   const date = new Date();
   const limit = new Date(todo.limit);
@@ -24,22 +24,20 @@ function RemainTimeCalcurate(todo) {
   const seconds = String(Math.floor((diff - hours * 1000 * 60 * 60 - minutes * 1000 * 60) / 1000)).padStart(2,"0");
   return [hours, minutes, seconds];
 }
-
+// Todo의 limit까지 남은시간을 출력하기 위한 함수
 function paintRemainTodo(toDolistLimit,todo) {
   const remains = RemainTimeCalcurate(todo);
   if (remains[0] < 0) {
-    toDolistLimit.innerText = "This To-Do is Time out!"
-  }else{
-    toDolistLimit.innerText = "Remains =" + `${remains[0]} : ${remains[1]} : ${remains[2]}`;
-  }
+    toDolistLimit.innerText = "This To-Do is Time out!"}else{
+    toDolistLimit.innerText = "Remains =" + `${remains[0]} : ${remains[1]} : ${remains[2]}`;}
 }
-
-function paintTodo(newTodo) {  // 새로운 Todo의 속성값을 저장 후 html로 가져오는 함수
-  const li = document.createElement("li");  // html에 li를 추가 후 변수로 저장
-  li.id = newTodo.id;  // li의 id는 newTodo의 id(현재시간값)
+// 새로운 Todo의 속성값을 저장 후 html로 가져오는 함수
+function paintTodo(newTodo) {  
+  const li = document.createElement("li");
+  li.id = newTodo.id;
   li.classList.add("todoStyle");
-  const span_input = document.createElement("span");  // html에 span을 추가 후 변수로 저장
-  span_input.innerText = newTodo.text;  // span 내부 글자는 newTodo의 text(입력값)
+  const span_input = document.createElement("span");
+  span_input.innerText = newTodo.text;
   span_input.classList.add("textBox");
   const span_importancy = document.createElement("span");
   span_importancy.innerText = "★".repeat(newTodo.important);
@@ -47,56 +45,57 @@ function paintTodo(newTodo) {  // 새로운 Todo의 속성값을 저장 후 html
   const span_limit = document.createElement("span");
   span_limit.classList.add("limitBox");
   paintRemainTodo(span_limit,newTodo)
-  const button = document.createElement("button");  // html에 button을 추가 후 변수로 저장
-  button.innerText = "❌";  // button 내부 글자는 ❌(삭제 표시)
-  button.addEventListener("click", deleteToDo);  // button을 클릭하는지 감지
-  li.appendChild(span_input);  // li에 span을 하위요소로 포함
-  li.appendChild(span_importancy);  // li에 span을 하위요소로 포함
-  li.appendChild(span_limit);  // li에 span을 하위요소로 포함
-  li.appendChild(button);  // li에 button을 하위요소로 포함
-  toDoList.appendChild(li);  // toDolist에 li를 하위요소로 포함
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span_input);
+  li.appendChild(span_importancy);
+  li.appendChild(span_limit);
+  li.appendChild(button);
+  toDoList.appendChild(li);
 }
-
-function handleToDoSubmit(event) {  // Todo를 Submit했을때 발생하는 일에 대한 함수
-  event.preventDefault();  // 기본값(새로고침)이 발생되지 않도록 함
-  const textTodo = toDoInput.value;  // textTodo에 현재 toDoForm에 입력된 input를 넣음
-  const limitTodo = toDoLimit.value;  // limitTodo에 현재 toDolimit에 입력된 input를 넣음
+// Todo를 Submit했을때 발생하는 함수 : Todo의 속성값을 저장하고, 화면에 출력함
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const textTodo = toDoInput.value;
+  const limitTodo = toDoLimit.value;
+  toDoInput.value = null;
+  toDoLimit.value = null;
   let selectedImportancy;
   toDoImportancy.forEach((star) => {
     if (star.checked){
       selectedImportancy = star.value;
       star.checked = false;
-    }
-  })
+    }})
   if (selectedImportancy == undefined){
     alert("중요도를 체크해주시기 바랍니다.");
-    return;
-  }
-  toDoInput.value = null;  // toDoForm의 input을 초기화함
-  toDoLimit.value = null;  // toDoForm의 limit을 초기화함
-  const newTodo = {  // newTodo의 객체를 생성
-    id: Date.now(),  // id : 현재시간값
-    text: textTodo,  // text : toDoForm 입력값
-    important: selectedImportancy,  // important : 선택된 중요도
-    limit: limitTodo  // limit : toDoLimit 입력값
-  }
-  paintTodo(newTodo);  //newTodo객체를 html에 추가
-  toDos.push(newTodo);  //newTodo객체를 toDos 배열에 추가
-  saveToDos();  // 현 toDos 배열을 localStorage에 저장
+    return;}
+  const newTodo = {
+    id: Date.now(),
+    text: textTodo,
+    important: selectedImportancy,
+    limit: limitTodo}
+  paintTodo(newTodo);
+  parsedToDos.push(newTodo);
+  saveToDos();
+  checkImg.onload = function(){
+    console.log("hi");
+    ctx.drawImage(checkImg,500,500,50,50);}
+};
+// ❌ 버튼을 클릭했을 때, Todo를 삭제하기 위한 함수
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  parsedToDos = parsedToDos.filter((todo) => todo.id != parseInt(li.id));
+  li.remove();
+  saveToDos();
 }
-
-function deleteToDo(event) {  // Todo를 삭제하기 위한 함수(변수 event는 특정사건이 발생했을때 생김)
-  const li = event.target.parentElement;  // 변수 li에 발생한 이벤트 내+ target(button)의 parent(li)를 저장
-  toDos = toDos.filter((todo) => todo.id != parseInt(li.id));  // 배열의 원소 중 todo와 li가 다른 id(시간값)인 경우에만 저장(parseInt는 str을 int로 변경)
-  li.remove();  // event가 발생한 todo의 li를 지워라
-  saveToDos();  // 현 Todos(event가 발생한 todo 제외하고 나머지)를 localStorage에 저장하는 함수
-}
-
+//-----------------------main 코드-------------------------------//
+// toDoForm이 submit되는지 감지하는 EventListener
 toDoForm.addEventListener("submit", handleToDoSubmit);
-
-let index = 0;
+// localStorage에 저장값이 있을때 이를 나타내기 위한 코드
 if (savedToDos !== null) {
   parsedToDos.forEach(paintTodo);
+  // 1초마다 남은 시간을 다시 출력하는 코드
   setInterval(() => {
     parsedToDos = JSON.parse(savedToDos);
     parsedToDos.forEach((todo) => {
