cbe44a59870c17b5dcf837dd75b7f61aeea22dba
Sangyeop Nam <spearoad15@gmail.com>
2023-03-16 22:39:44 +0900

    feat : 애니메이션으로 Todo 위치 구현
    - Todo의 위치 좌표를 시간과 중요도로 계산하여 그래프에 그림
    - 브라우저의 애니메이션 동작을 위해 requestAnimationFrame함수 사용
    - 애니메이션 활용하여 UI가 원활히 API 활용할 수 있도록 할 예

--- js/graph.js
+++ js/graph.js
@@ -10,65 +10,77 @@ const checkSize = 20;
 canvas.width = graphWidth;
 canvas.height = graphHeight;
 const ctx = canvas.getContext('2d');
-
+// 애니메이션을 위한 초기화
 let x = 100;
-let y = 100;
-let dx = 10;
-let dy = 10;
+let dx = 1;
 const radius = 20;
-
+let isMouseOnCircle = false;
+let nowId
+let nowTodoX
+let nowTodoY
+ctx.font = "30px Arial";
+// Frame 그리는 함수
 function drawFrame() {
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    // 세로줄
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // 격자
     ctx.strokeStyle = "green";
     ctx.lineWidth = 1;
-    for (let i = 1; i <= 24; i++){
+    x -= dx;
+    if (x - radius < 0) {
+        x = graphWidth;
+    }
+    for (let i = 1; i <= 48; i++) {
         ctx.beginPath();
-        ctx.moveTo(graphWidth/25*i, 0);
-        ctx.lineTo(graphWidth/25*i, graphHeight);
+        ctx.moveTo(x + (i - 24) * graphWidth / 24, 0);
+        ctx.lineTo(x + (i - 24) * graphWidth / 24, graphHeight);
+        ctx.stroke()
+    }
+    for (let i = 1; i <= 10; i++) {
+        ctx.beginPath();
+        ctx.moveTo(0, graphHeight / 10 * (2 * i - 1));
+        ctx.lineTo(graphWidth, graphHeight / 10 * (2 * i - 1));
         ctx.stroke();
     }
-    for (let i = 1; i <= 10; i++){
+    // Check 표시
+    parsedToDos = JSON.parse(savedToDos);
+    let locationTodos = []
+    parsedToDos.forEach((parsedToDo) => locationTodos.push(calLocationTodo(parsedToDo)));
+    locationTodos.forEach(function (locationTodo) {
+        circleX = locationTodo[0] + x
+        circleY = locationTodo[1]
         ctx.beginPath();
-        ctx.moveTo(0, graphHeight/10*(2*i-1));
-        ctx.lineTo(graphWidth, graphHeight/10*(2*i-1));
-        ctx.stroke();
+        ctx.arc(circleX, circleY, radius, 0, Math.PI * 2);
+        ctx.fillStyle = 'red';
+        ctx.fill();
+    })
+    // 마우스 상호작용
+    canvas.addEventListener("mousemove", function (event) {
+        let rect = canvas.getBoundingClientRect();
+        locationTodos.forEach(function (locationTodo) {
+            circleX = locationTodo[0] + x
+            circleY = locationTodo[1]
+            let eventX = event.clientX - rect.left;
+            let eventY = event.clientY - rect.top;
+            let distance = Math.sqrt(Math.pow(eventX - circleX, 2) + Math.pow(eventY - circleY, 2));
+            if (distance <= radius && !isMouseOnCircle) {
+                isMouseOnCircle = true;
+                nowId = locationTodo[2];
+                nowTodoX = eventX;
+                nowTodoY = eventY;
+            }
+        })
+    })
+    if (isMouseOnCircle) {
+        ctx.fillText(nowId, nowTodoX, nowTodoY);
     }
-    x += dx;
-    y += dy;
-    if (x + radius > canvas.width || x - radius < 0) {
-        dx = -dx;
-      }
-    if (y + radius > canvas.height || y - radius < 0) {
-        dy = -dy;
-      }
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2);
-    ctx.fillStyle = 'red';
-    ctx.fill();
     requestAnimationFrame(drawFrame);
 }
-
-function calLocationTodo(todo){ // Todo 위치를 계산하기 위한 함수(변수는 todo객체)
+// Todo 위치를 계산하기 위한 함수(변수는 todo객체)
+function calLocationTodo(todo) {
     const remains = RemainTimeCalcurate(todo);
-    const locationX = graphWidth * (Number(remains[0])*60+Number(remains[1])) / (26*60);
-    const locationY = graphHeight * (9-(todo.important-1)*2) / 10;
-    return [locationX, locationY];
+    const locationX = graphWidth * (Number(remains[0]) * 60 + Number(remains[1])) / (26 * 60);
+    const locationY = graphHeight * (9 - (todo.important - 1) * 2) / 10;
+    return [locationX, locationY, todo.id];
 }
 
-
-
 requestAnimationFrame(drawFrame)
-//Todo 그리기(원)
-// parsedToDos = JSON.parse(savedToDos);
-// let locationTodos = []
-// parsedToDos.forEach((parsedTodo) => locationTodos.push(calLocationTodo(parsedTodo)));
-// locationTodos.forEach(function(locationTodo) {
-//     ctx.strokeStyle = "red";
-//     ctx.lineWidth = 10;
-//     ctx.beginPath();
-//     ctx.arc(locationTodo[0],locationTodo[1],checkSize,0,Math.PI*2);
-//     ctx.fillStyle = "yellow";
-//     ctx.fill();
-//     ctx.closePath();
-// })
--- js/todo.js
+++ js/todo.js
@@ -6,10 +6,10 @@ const toDoImportancy = stars.querySelectorAll("input[type='radio']");
 const toDoLimit = toDoForm.querySelector("input[name='todo-limit']");
 const toDoList = document.querySelector("#todo-list");
 // localStorage에 있는 Todo를 불러와 변수로 저장
-const TODOS_KEY = "todos"; 
-let savedToDos = localStorage.getItem(TODOS_KEY); 
+const TODOS_KEY = "todos";
+let savedToDos = localStorage.getItem(TODOS_KEY);
 let parsedToDos = JSON.parse(savedToDos);
- // Todo를 localStorage에 저장하기 위한 함수
+// Todo를 localStorage에 저장하기 위한 함수
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(parsedToDos));
   savedToDos = localStorage.getItem(TODOS_KEY);
@@ -19,20 +19,22 @@ function RemainTimeCalcurate(todo) {
   const date = new Date();
   const limit = new Date(todo.limit);
   const diff = limit.getTime() - date.getTime();
-  const hours = String(Math.floor(diff / (1000 * 60 * 60))).padStart(2,"0");
-  const minutes = String(Math.floor((diff - hours * 1000 * 60 * 60) / (1000 * 60))).padStart(2,"0");
-  const seconds = String(Math.floor((diff - hours * 1000 * 60 * 60 - minutes * 1000 * 60) / 1000)).padStart(2,"0");
+  const hours = String(Math.floor(diff / (1000 * 60 * 60))).padStart(2, "0");
+  const minutes = String(Math.floor((diff - hours * 1000 * 60 * 60) / (1000 * 60))).padStart(2, "0");
+  const seconds = String(Math.floor((diff - hours * 1000 * 60 * 60 - minutes * 1000 * 60) / 1000)).padStart(2, "0");
   return [hours, minutes, seconds];
 }
 // Todo의 limit까지 남은시간을 출력하기 위한 함수
-function paintRemainTodo(toDolistLimit,todo) {
+function paintRemainTodo(toDolistLimit, todo) {
   const remains = RemainTimeCalcurate(todo);
   if (remains[0] < 0) {
-    toDolistLimit.innerText = "This To-Do is Time out!"}else{
-    toDolistLimit.innerText = "Remains =" + `${remains[0]} : ${remains[1]} : ${remains[2]}`;}
+    toDolistLimit.innerText = "Timeover =" + `${remains[0]} : ${remains[1]} : ${remains[2]}`;
+  } else {
+    toDolistLimit.innerText = "Remains =" + `${remains[0]} : ${remains[1]} : ${remains[2]}`;
+  }
 }
 // 새로운 Todo의 속성값을 저장 후 html로 가져오는 함수
-function paintTodo(newTodo) {  
+function paintTodo(newTodo) {
   const li = document.createElement("li");
   li.id = newTodo.id;
   li.classList.add("todoStyle");
@@ -44,7 +46,7 @@ function paintTodo(newTodo) {
   span_importancy.classList.add("importancyBox");
   const span_limit = document.createElement("span");
   span_limit.classList.add("limitBox");
-  paintRemainTodo(span_limit,newTodo)
+  paintRemainTodo(span_limit, newTodo)
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
@@ -63,24 +65,28 @@ function handleToDoSubmit(event) {
   toDoLimit.value = null;
   let selectedImportancy;
   toDoImportancy.forEach((star) => {
-    if (star.checked){
+    if (star.checked) {
       selectedImportancy = star.value;
       star.checked = false;
-    }})
-  if (selectedImportancy == undefined){
+    }
+  })
+  if (selectedImportancy == undefined) {
     alert("중요도를 체크해주시기 바랍니다.");
-    return;}
+    return;
+  }
   const newTodo = {
     id: Date.now(),
     text: textTodo,
     important: selectedImportancy,
-    limit: limitTodo}
+    limit: limitTodo
+  }
   paintTodo(newTodo);
   parsedToDos.push(newTodo);
   saveToDos();
-  checkImg.onload = function(){
+  checkImg.onload = function () {
     console.log("hi");
-    ctx.drawImage(checkImg,500,500,50,50);}
+    ctx.drawImage(checkImg, 500, 500, 50, 50);
+  }
 };
 // ❌ 버튼을 클릭했을 때, Todo를 삭제하기 위한 함수
 function deleteToDo(event) {
@@ -100,6 +106,7 @@ if (savedToDos !== null) {
     parsedToDos = JSON.parse(savedToDos);
     parsedToDos.forEach((todo) => {
       const toDolistLimit = toDoList.querySelector(`li[id="${String(todo.id)}"] > span[class='limitBox']`);
-      paintRemainTodo(toDolistLimit,todo);
-    })},1000)
+      paintRemainTodo(toDolistLimit, todo);
+    })
+  }, 1000)
 }
